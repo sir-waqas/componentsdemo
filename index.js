3d085085b7ecc4219469276bd9f98c35b028b4d6
Waqas Azam <waqas.mabsapps@gmail.com>
2019-03-13 20:07:18 +0500

    Adding Footer Component

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
-// /**
-//  * @format
-//  */
-
-// import {AppRegistry} from 'react-native';
-// import App from './App';
-// import {name as appName} from './app.json';
-
-// AppRegistry.registerComponent(appName, () => App);
-
 //Import App Registry, View and Text from React Native Library
 import { AppRegistry, View, Text } from 'react-native';
 //Import React for Behaviours from React
 import React from 'react';
+//Import the Header and Footer file to use it in the Root View
+import Header from './src/components/Header';
+import Footer from './src/components/Footer';
 
 //Root Functional Component to Init the App
 const ComponentsDemo = () => (
     //First View with Styling to center items containing a Text View 
-    <View style={{ flex: 1 }}>
+    <View style={{ flex: 1, alignContent: "center", alignContent: "center" }}>
         {/* <Text>My Resume</Text> */}
         <Header headerText={'Resume App'} />
+        {/* Main Resume Content to be inserted here */}
+        <Footer footerText={'CopyRights 2019'} />
     </View>
 );
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,16 +3,37 @@ import { Text, View } from 'react-native';
 import React from 'react';
 
 //Prepare the contents
-const Header = (props) => {
+const Footer = (props) => {
+    //Destructuring the styles
+    const { textStyle, viewStyle } = styles;
+
     return (
-        <View>
-            <Text>{props.headerText}</Text>
+        <View style={viewStyle}>
+            <Text style={textStyle}>{props.footerText}</Text>
         </View>
     );
 };
 
 //Style the contents
-const styles = {};
+const styles = {
+    viewStyle: {
+        flexDirection: 'row',
+        alignItems: "center",
+        justifyConents: "center",
+        backgroundColor: '#F8F8F8',
+        marginBottom: 15,
+        height: 30,
+        paddingBottom: 15,
+        position: "absolute", // To make it at the bottom
+        bottom: 0 // To make it at the bottom
+    },
+    textStyle: {
+        // alignSelf: 'center', // making text appear in center
+        textAlign: 'center',
+        fontSize: 12,
+        backgroundColor: 'white'
+    }
+};
 
 //Make them available to the app
-export default Header;
+export default Footer;
