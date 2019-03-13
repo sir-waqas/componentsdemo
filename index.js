eb3a2ca8caf7f1bdf3fcef8bbd4052a76546f1a7
Waqas Azam <waqas.mabsapps@gmail.com>
2019-03-13 17:55:45 +0500

    First Basic Component in the Screen Center

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-/**
- * @format
- */
+// /**
+//  * @format
+//  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+// import {AppRegistry} from 'react-native';
+// import App from './App';
+// import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+// AppRegistry.registerComponent(appName, () => App);
+
+//Import App Registry, View and Text from React Native Library
+import { AppRegistry, View, Text } from 'react-native';
+//Import React for Behaviours from React
+import React from 'react';
+
+//Root Functional Component to Init the App
+const ComponentsDemo = () => (
+    //First View with Styling to center items containing a Text View 
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>My Resume</Text>
+    </View>
+);
+
+AppRegistry.registerComponent('componentsdemo', () => ComponentsDemo);
