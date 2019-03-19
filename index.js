a852a6ea2515e4e3d5ffd9bd47be66bfa5ea7e0e
Waqas Azam <waqas.mabsapps@gmail.com>
2019-03-19 21:56:27 +0500

    Adjusting the scroll view

--- index.js
+++ index.js
@@ -13,19 +13,21 @@ import Footer from './src/components/Footer';
 //Root Functional Component to Init the App
 const ComponentsDemo = () => (
     //First View with Styling to center items containing a Text View 
-    <ScrollView style={{ flex: 1, flexDirection: 'column' }}>
+    <View style={{ flex: 1, flexDirection: 'column' }}>
 
         {/* <Text>My Resume</Text> */}
-        <Header headerText={'Personal Information App'} />
-        {/* Main Resume Content to be inserted here */}
         <View>
+            <Header headerText={'Personal Information App'} />
+        </View>
+        {/* Main Resume Content to be inserted here */}
+        <ScrollView>
             <ProfilePic />
             <Summary summaryHeading={'Summary'} summary={'Summary place holder'} />
             <Experience expHeading={'Experience'} experience={'Placeholder'} />
             <Education eduHeading={'Education'} education={'Placeholder'} />
             <Footer footerText={'CopyRights 2019'} />
-        </View>
-    </ScrollView>
+        </ScrollView>
+    </View>
 );
 
 AppRegistry.registerComponent('componentsdemo', () => ComponentsDemo);
